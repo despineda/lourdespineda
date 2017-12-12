cab29a24b9d91451f1336ef279ad6db7da3d37ac
des <des.pineda@rocketmail.com>
2017-12-12 21:36:55 +0800

    javascript

---
+++ mod05-02/js/script.js
@@ -0,0 +1,23 @@
+function redfunc(){
+	document.getElementById('red-box').style.backgroundColor = "red";
+}
+
+function redfunc(){
+	document.getElementById('orange-box').style.backgroundColor = "orange";
+}
+
+function redfunc(){
+	document.getElementById('yellow-box').style.backgroundColor = "yellow";
+}
+
+function redfunc(){
+	document.getElementById('green-box').style.backgroundColor = "green";
+}
+
+function redfunc(){
+	document.getElementById('purple-box').style.backgroundColor = "purple";
+}
+
+function redfunc(){
+	document.getElementById('blue-box').style.backgroundColor = "red";
+}
